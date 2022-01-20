1182c53d042cd8b71d4ef1f3dee0089c0ca946a6
Ricardo Francé <ricardolfrance@gmail.com>
2022-01-20 16:29:41 -0300

    type exercices

---
+++ typescript/index.ts
@@ -0,0 +1,48 @@
+// types
+// interfaces
+/*
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    domestico: boolean;
+}
+
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+interface ICanino extends IAnimal {
+    porte: 'pequeno' | 'médio' | 'grande';
+}
+
+type IDomestico = IFelino | ICanino;
+
+const animal: IDomestico = {
+    nome: 'cachorro',
+    porte: 'médio',   
+    tipo: 'terrestre',
+    domestico: true
+}
+
+animal.executarRugido('s')
+
+
+const felino: IFelino = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true
+}
+*/
+
+/*
+const input = document.getElementById('input') as HTMLInputElement;
+
+input.addEventListener('input', (event) => {
+    
+    const i = event.currentTarget as HTMLInputElement;
+});
+*/
+
+//Generic types
+
