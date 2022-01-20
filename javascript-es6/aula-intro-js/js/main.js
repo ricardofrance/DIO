62dfa2684cf1251947bb76d2323eeda03ed7335f
Ricardo Francé <ricardolfrance@gmail.com>
2022-01-20 14:13:00 -0300

    teste

--- javascript-es6/aula-intro-js/js/main.js
+++ javascript-es6/aula-intro-js/js/main.js
@@ -112,3 +112,34 @@ function load (){
 function funcaoChange(elemento){
     console.log(elemento.value);
 }
+
+
+/*
+let valoresPares = 0;
+let valoresImpares = 0;
+let valoresPositivos = 0;
+let valoresNegativos = 0;
+
+for (var i = 0; i < 5; i++) {
+  const valorInformadoPeloUsuario = parseInt(gets());
+
+  //  Criar a condição adequada para verificar se o "valorInformadoPeloUsuario" é par.
+  if (valorInformadoPeloUsuario % 2 === 0) {
+    valoresPares++;
+  } else {
+    valoresImpares++;
+  }
+
+  if (valorInformadoPeloUsuario > 0) {
+    valoresPositivos++;
+  } else if (valorInformadoPeloUsuario < 0) {
+    valoresNegativos++;
+  }
+
+}
+
+console.log(`${valoresPares} par(es)`);
+console.log(`${valoresImpares} impar(es)`);
+console.log(`${valoresPositivos} positivo(s)`);
+console.log(`${valoresNegativos} negativo(s)`);
+*/
